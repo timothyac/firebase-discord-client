8e7628948d3257de4915dbcb8d550366c7a07dea
SunstroUS <Sunstrous@gmail.com>
2020-08-17 22:10:45 -0400

    Discord oauth plus storing tokens

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,37 @@
 import React, { useEffect, useState, useContext } from "react";
+import { useLocation, useHistory } from "react-router-dom";
 
 import { auth, db } from "../firebase";
 import { AuthStateContext } from "../context/AuthContext";
 
-const Dashboard = () => {
-  // Get user's unique ID
-  const { uid } = useContext(AuthStateContext);
+const useQuery = () => {
+  return new URLSearchParams(useLocation().search);
+};
+
+const realCode = (code: string) => /^[a-zA-Z0-9]{30}$/.test(code);
+
+const SERVERLESS_URL =
+  "http://localhost:5001/dashboard-auth-a8d93/us-central1/helloWorld";
+
+// POST to serverless function for token
+const requestAccessToken = async (code: string) => {
+  try {
+    // Request access code from discord
+    const res = await fetch(`${SERVERLESS_URL}?code=${code}`, {
+      method: "POST",
+    });
+
+    const json = await res.json();
+
+    if (json.error) return Promise.reject(json.error_description);
+
+    return json;
+  } catch (error) {
+    return new Error(error);
+  }
+};
+
+const UserId = ({ uid }: any) => {
   const [id, setId] = useState(null);
 
   // Get user data from Firebase
@@ -24,14 +50,43 @@ const Dashboard = () => {
     getUserData();
   }, []);
 
+  return <pre>{id}</pre>;
+};
+
+const Dashboard = () => {
+  // Get user's unique ID
+  const { uid } = useContext(AuthStateContext);
+  let history = useHistory();
+  const query = useQuery();
+  const code = query.get("code") as string;
+
+  // Check for valid access code
+  if (code && realCode(code)) {
+    requestAccessToken(code)
+      // Store tokens in firestore
+      .then((json) => db.collection("users").doc(uid).set(json))
+      // Redirect to dashboard
+      .then(() => history.push("/dashboard"))
+      .catch((err) => {
+        if (err === 'Invalid "code" in request.') {
+          // Send back to dashboard
+          history.push("/dashboard");
+        } else {
+          console.error(err);
+          alert(err);
+        }
+      });
+  }
+
   return (
     <div>
       <h1>Dashboard</h1>
-      <pre>{id}</pre>
+      <UserId uid={uid} />
       <button onClick={() => auth.signOut()}>Sign out</button>
-      <a href="https://discord.com/api/oauth2/authorize?client_id=744685186335244299&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fdashboard&response_type=code&scope=identify%20email">
+      <a href="https://discord.com/api/oauth2/authorize?client_id=744685186335244299&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fdashboard&response_type=code&scope=identify">
         <button>Discord Login</button>
       </a>
+      {query.get("code") && <em>Saving Token</em>}
     </div>
   );
 };
